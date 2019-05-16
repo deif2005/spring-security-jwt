d37133dd96e0b135f6470bb401c9bef94104678d
defi2015 <11107040@qq.com>
2019-05-16 17:30:44 +0800

    1

---
+++ activate-common/src/main/java/com/order/machine/common_const/CommonConst.java
@@ -0,0 +1,15 @@
+package com.order.machine.common_const;
+
+/**
+ * @author miou
+ * @date 2018-08-30
+ */
+public class CommonConst {
+    public static final String JYPACKAGEDIR="/jy_package/";
+
+    public static final String CHAPTER_ALL="CHAPTER_ALL";
+
+    public static final String CHAPTER_NONE="CHAPTER_NONE";
+
+    public static final String LOGIN_KEY="LOGIN_KEY";
+}
