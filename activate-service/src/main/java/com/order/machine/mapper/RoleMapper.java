289b2c116207022599d17c87591fadd7f9d20ded
defi2015 <11107040@qq.com>
2019-07-19 18:49:48 +0800

    增加菜单控制

--- activate-service/src/main/java/com/order/machine/mapper/RoleMapper.java
+++ activate-service/src/main/java/com/order/machine/mapper/RoleMapper.java
@@ -1,5 +1,6 @@
 package com.order.machine.mapper;
 
+import com.order.machine.po.MenuPo;
 import com.order.machine.po.RolePo;
 import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
@@ -15,4 +16,6 @@ public interface RoleMapper extends Mapper<RolePo>{
     List<RolePo> getRoleByUserName(String userName);
 
     List<RolePo> getRoleByResource(@Param("resourceId") Integer resourceId);
+
+    List<MenuPo> getMenuResourceByUserName(String userName);
 }
